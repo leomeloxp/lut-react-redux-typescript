2190375c265ab905974b36d6842697b3a40e7393
Leonardo Melo <hello@leomeloxp.net>
2019-08-28 20:47:02 +0100

    Finish video 17
    - Connect `MoviesList` component to Redux store

--- src/movies/MoviesList.tsx
+++ src/movies/MoviesList.tsx
@@ -1,32 +1,24 @@
-/* eslint react/no-did-mount-set-state: 0 */
 import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { AnyAction, bindActionCreators, Dispatch } from 'redux';
 import styled from 'styled-components';
+import { AppState } from '../helpers/rootReducer';
+import { getMovies } from './actions';
 import Movie from './Movie';
 import { IMovie } from './reducer';
 
-class MoviesList extends PureComponent<{}, { movies: IMovie[] }> {
-  state = {
-    movies: []
-  };
-
-  async componentDidMount() {
-    try {
-      const res = await fetch(
-        'https://api.themoviedb.org/3/discover/movie?api_key=65e043c24785898be00b4abc12fcdaae&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
-      );
-      const movies = await res.json();
-      this.setState({
-        movies: movies.results
-      });
-    } catch (e) {
-      console.log(e);
-    }
+class MoviesList extends PureComponent<{ movies: IMovie[]; getMovies: typeof getMovies }, {}> {
+  public componentDidMount() {
+    const { getMovies } = this.props;
+    getMovies();
+    //this.props.getMovies();
   }
 
-  render() {
+  public render() {
+    const { movies } = this.props;
     return (
       <MovieGrid>
-        {this.state.movies.map((movie: IMovie) => (
+        {movies.map((movie: IMovie) => (
           <Movie key={movie.id} movie={movie} />
         ))}
       </MovieGrid>
@@ -34,7 +26,22 @@ class MoviesList extends PureComponent<{}, { movies: IMovie[] }> {
   }
 }
 
-export default MoviesList;
+const mapStateToProps = (state: AppState) => ({
+  movies: state.movies.movies
+});
+
+const mapDispatchToProps = (dispatch: Dispatch<AnyAction>) =>
+  bindActionCreators(
+    {
+      getMovies
+    },
+    dispatch
+  );
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MoviesList);
 
 const MovieGrid = styled.div`
   display: grid;
--- src/toggle/Toggle.tsx
+++ src/toggle/Toggle.tsx
@@ -2,18 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { ActionCreator, bindActionCreators, Dispatch } from 'redux';
 import { AppState } from '../helpers/rootReducer';
-import { getMovies } from '../movies/actions';
 import { IReduxToggleMessageAction, toggleMessage } from './actions';
 
 const Toggle: React.FC<{
   messageVisibility: boolean;
   toggleMessage: ActionCreator<IReduxToggleMessageAction>;
-  getMovies: typeof getMovies;
-}> = ({ getMovies, messageVisibility, toggleMessage }) => (
+}> = ({ messageVisibility, toggleMessage }) => (
   <div>
     {messageVisibility && <p>You will be seeing this if redux action is toggled</p>}
     <button onClick={toggleMessage}>Toggle Me</button>
-    <button onClick={getMovies}>Load Movies</button>
   </div>
 );
 
@@ -22,7 +19,7 @@ const mapStateToProps = (state: AppState) => ({
 });
 
 const mapDispatchToProps = (dispatch: Dispatch<IReduxToggleMessageAction>) =>
-  bindActionCreators({ toggleMessage, getMovies }, dispatch);
+  bindActionCreators({ toggleMessage }, dispatch);
 
 export default connect(
   mapStateToProps,
